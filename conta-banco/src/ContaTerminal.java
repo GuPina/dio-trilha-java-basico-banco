3f7fa865410afc98caf718b4c7b18be17638e747
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2024-08-21 18:29:58 -0300

    commit final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua Agência: ");
+        String agenciaDigitada = scanner.nextLine();
+
+        if(agenciaDigitada.equals(UsuarioConta.agencia)) {
+            System.out.println("Agência correta!");
+            System.out.println(UsuarioConta.conta +  " (Digite enter para continuar...)");
+            scanner.nextLine();
+
+            System.out.println("Olá " + UsuarioConta.nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaDigitada + ", conta número " + UsuarioConta.conta + ". Seu saldo de " + UsuarioConta.saldo + " já está disponível para saque.");
+        } else {
+            System.out.println("Agência incorreta! A agência digitada não corresponde a conta nessa agência, favor verificar a agência correta.");
+        }
     }
 }
---
+++ conta-banco/src/UsuarioConta.java
@@ -0,0 +1,8 @@
+public class UsuarioConta {
+
+    static String agencia = "0001";
+    static int conta = 1534;
+    static String nome = "Gustavo Pina";
+    static float saldo = (float) 1000.00;
+    
+}
